8ae9f40eb3a9906dc0ad5d5b2d7b05ad6bcce642
Bartosz <radu480@o2.pl>
2021-01-02 18:15:46 +0100

    Prepared db context.

---
+++ GarageManager.Data/Context/GarageManagerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using GarageManager.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManager.Data.Context
+{
+    public class GarageManagerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public GarageManagerDbContext(DbContextOptions options) : base(options) { }
+
+    }
+}
