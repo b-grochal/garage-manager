0e5f16dea5a7b2e409c344e0a2fa4850b49f37d9
Bartosz <radu480@o2.pl>
2021-01-02 17:10:47 +0100

    Prepared domain entities.

---
+++ GarageManager.Data/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManager.Data.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public ICollection<Car> Cars { get; set; }
+    }
+}
---
+++ GarageManager.Data/Entities/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManager.Data.Entities
+{
+    public class Service
+    {
+        public int ServiceId { get; set; }
+        public int CarId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Cosr { get; set; }
+        public DateTime DateOfService { get; set; }
+        public Car Car { get; set; }
+    }
+}
