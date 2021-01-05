e3bc244ef6c23cf9cb7cf2f4aedaa80c05255f30
Bartosz <radu480@o2.pl>
2021-01-05 17:14:42 +0100

    Modified application main class.

--- GarageManager.UI/App.xaml.cs
+++ GarageManager.UI/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,47 @@ namespace GarageManager.UI
     /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = CreateHostBuilder().Build();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args = null)
+        {
+            return Host.CreateDefaultBuilder(args);
+                //.AddConfiguration()
+                //.AddFinanceAPI()
+                //.AddDbContext()
+                //.AddServices()
+                //.AddStores()
+                //.AddViewModels()
+                //.AddViews();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            //SimpleTraderDbContextFactory contextFactory = _host.Services.GetRequiredService<SimpleTraderDbContextFactory>();
+            //using (SimpleTraderDbContext context = contextFactory.CreateDbContext())
+            //{
+            //    context.Database.Migrate();
+            //}
+
+            Window window = _host.Services.GetRequiredService<MainWindow>();
+            window.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
     }
 }
