8c048ae89bc17a1b9807f5614177521ecbe3b5b2
Bartosz <radu480@o2.pl>
2021-01-06 16:15:36 +0100

    Prepared authenitcator without logic.

--- GarageManager.UI/State/Authenticator/Authenticator.cs
+++ GarageManager.UI/State/Authenticator/Authenticator.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.State.Authenticator
 {
     public class Authenticator : IAuthenticator
     {
+        public User LoggedUser => throw new NotImplementedException();
+
+        public bool IsLoggedIn => throw new NotImplementedException();
+
+        public event Action StateChanged;
+
+        public Task Login(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Logout()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- GarageManager.UI/State/Authenticator/IAuthenticator.cs
+++ GarageManager.UI/State/Authenticator/IAuthenticator.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.State.Authenticator
 {
     public interface IAuthenticator
     {
+        User LoggedUser{ get; }
+        bool IsLoggedIn { get; }
+
+        event Action StateChanged;
+
+        Task Login(string login, string password);
+
+        void Logout();
     }
 }
