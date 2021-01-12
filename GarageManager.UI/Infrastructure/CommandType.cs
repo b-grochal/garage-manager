3670bb6f3e6565e67ed1e6f228df32105efcae18
Bartosz <radu480@o2.pl>
2021-01-12 16:38:34 +0100

    Prepared factory for commands.

---
+++ GarageManager.UI/Infrastructure/CommandType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManager.UI.Infrastructure
+{
+    public enum CommandType
+    {
+        Login,
+        ShowHomePage,
+        ShowUsersList
+    }
+}
---
+++ GarageManager.UI/Infrastructure/ICommandFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace GarageManager.UI.Infrastructure
+{
+    public interface ICommandFactory
+    {
+        ICommand CreateViewModel(CommandType viewType);
+    }
+}
