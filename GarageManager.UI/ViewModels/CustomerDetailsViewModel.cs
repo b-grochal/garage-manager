3f36203044bb0d427197c9cbc0e268739e8e15d9
Bartosz <radu480@o2.pl>
2021-01-18 17:49:21 +0100

    Prepared details view for customers.

--- GarageManager.UI/ViewModels/CustomerDetailsViewModel.cs
+++ GarageManager.UI/ViewModels/CustomerDetailsViewModel.cs
@@ -10,6 +10,7 @@ namespace GarageManager.UI.ViewModels
         #region Fields
 
         private Customer customer;
+        private Car selectedCustomerCar;
 
         #endregion Fields
 
@@ -27,6 +28,87 @@ namespace GarageManager.UI.ViewModels
             }
         }
 
+        public string FirstName
+        {
+            get
+            {
+                return this.customer.FirstName;
+            }
+            set
+            {
+                this.customer.FirstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.customer.LastName;
+            }
+            set
+            {
+                this.customer.LastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.customer.PhoneNumber;
+            }
+            set
+            {
+                this.customer.PhoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return this.customer.Email;
+            }
+            set
+            {
+                this.customer.Email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        public IEnumerable<Car> CustomerCars
+        {
+            get
+            {
+                return this.customer.Cars;
+            }
+        }
+
+        public Car SelectedCustomerCar
+        {
+            get
+            {
+                return this.selectedCustomerCar;
+            }
+            set
+            {
+                this.selectedCustomerCar = value;
+                OnPropertyChanged(nameof(SelectedCustomerCar));
+            }
+        }
+
         #endregion Properties
+
+        #region Constructors
+
+        public CustomerDetailsViewModel()
+        {
+
+        }
+
+        #endregion Constructors
     }
 }
