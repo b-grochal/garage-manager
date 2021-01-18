4d252e84951725665342a3d31d2e17314b8b2fdf
Bartosz <radu480@o2.pl>
2021-01-18 18:56:40 +0100

    Created service for cars.

---
+++ GarageManager.Services/Interfaces/ICarsService.cs
@@ -0,0 +1,19 @@
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.SearchCriteria;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageManager.Services.Interfaces
+{
+    public interface ICarsService
+    {
+        Task<IEnumerable<Car>> GetCars();
+        Task<IEnumerable<Car>> GetCars(CarsListSearchCriteria customersListSearchCriteria);
+        Task<Car> GetCar(int carId);
+        Task EditCar(Car car);
+        Task DeleteCar(int carId);
+        Task CreateCustomer(Car car);
+    }
+}
---
+++ GarageManager.Services/SearchCriteria/CarsListSearchCriteria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManager.Services.SearchCriteria
+{
+    public class CarsListSearchCriteria
+    {
+        public string VIN { get; set; }
+    }
+}
