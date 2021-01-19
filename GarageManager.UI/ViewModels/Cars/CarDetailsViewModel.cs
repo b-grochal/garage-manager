c9a3c50904c5fc5197848a1422897672b0591a97
Bartosz <radu480@o2.pl>
2021-01-19 16:35:22 +0100

    Prepared viewmodel for car details.

--- GarageManager.UI/ViewModels/Cars/CarDetailsViewModel.cs
+++ GarageManager.UI/ViewModels/Cars/CarDetailsViewModel.cs
@@ -1,10 +1,98 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GarageManager.UI.ViewModels
 {
-    class CarDetailsViewModel
+    public class CarDetailsViewModel : BaseViewModel
     {
+        #region Fields
+
+        private Car car;
+        private Service selectedService;
+
+        #endregion Fields
+
+        #region Properties
+
+        public Car Car
+        {
+            get
+            {
+                return this.car;
+            }
+            set
+            {
+                this.car = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return this.car.Model;
+            }
+        }
+
+        public string Brand
+        {
+            get
+            {
+                return this.car.Brand;
+            }
+        }
+
+        public string Vin
+        {
+            get
+            {
+                return this.car.VIN;
+            }
+        }
+
+        public string RegistrationNumber
+        {
+            get
+            {
+                return this.car.RegistrationNumber;
+            }
+        }
+
+        public string FuleType
+        {
+            get
+            {
+                return this.car.FuelType;
+            }
+        }
+
+        public string Engine
+        {
+            get
+            {
+                return this.car.Engine;
+            }
+        }
+
+        public string Transmission
+        {
+            get
+            {
+                return this.car.Transmission;
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public CarDetailsViewModel()
+        {
+
+        }
+
+        #endregion Constructors
     }
 }
