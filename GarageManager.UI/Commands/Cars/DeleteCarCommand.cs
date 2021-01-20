2d78a3ad273b17b19cd4157319b1ef049f11979b
Bartosz <radu480@o2.pl>
2021-01-20 11:48:13 +0100

    Implemented command for deleting cars.

--- GarageManager.UI/Commands/Cars/DeleteCarCommand.cs
+++ GarageManager.UI/Commands/Cars/DeleteCarCommand.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.Commands.Cars
 {
-    class DeleteCarCommand
+    public class DeleteCarCommand : BaseAsyncCommand
     {
+        private readonly CarsListViewModel carsListViewModel;
+        private readonly ICarsService carsService;
+
+        public DeleteCarCommand(CarsListViewModel carsListViewModel, ICarsService carsService)
+        {
+            this.carsListViewModel = carsListViewModel;
+            this.carsService = carsService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            await carsService.DeleteCar(carsListViewModel.SelectedCar.CarId);
+            IEnumerable<Car> cars = await carsService.GetCars();
+            carsListViewModel.Cars = cars;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return !IsExecuting && this.carsListViewModel.SelectedCar!= null;
+        }
     }
 }
