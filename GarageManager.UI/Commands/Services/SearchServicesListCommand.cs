ef7b79598a9f8a1359beda1e56c792ab1463564a
Bartosz <radu480@o2.pl>
2021-01-20 23:49:05 +0100

    Implemented method for filtering list of services.

--- GarageManager.UI/Commands/Services/SearchServicesListCommand.cs
+++ GarageManager.UI/Commands/Services/SearchServicesListCommand.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.Commands.Services
 {
-    class SearchServicesListCommand
+    public class SearchServicesListCommand : BaseAsyncCommand
     {
+        private readonly ServicesListViewModel servicesListViewModel;
+        private readonly IServicesService servicesService;
+
+        public SearchServicesListCommand(ServicesListViewModel servicesListViewModel, IServicesService servicesService)
+        {
+            this.servicesListViewModel = servicesListViewModel;
+            this.servicesService = servicesService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            IEnumerable<Service> filteredServices = await servicesService.GetServices(servicesListViewModel.ServicesListSearchCriteria);
+            servicesListViewModel.Services = filteredServices;
+        }
     }
 }
