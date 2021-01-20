7c82179338508f454fb10e6e8982cd40cd67182b
Bartosz <radu480@o2.pl>
2021-01-20 12:24:31 +0100

    Implemented command for showing list of cars.

--- GarageManager.UI/Commands/Cars/ShowCarsListCommand.cs
+++ GarageManager.UI/Commands/Cars/ShowCarsListCommand.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.Infrastructure;
+using GarageManager.UI.State.Navigator;
+using GarageManager.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.Commands.Cars
 {
-    class ShowCarsListCommand
+    public class ShowCarsListCommand : BaseAsyncCommand
     {
+        private readonly ICarsService carsService;
+        private readonly INavigator navigator;
+        private readonly IViewModelFactory viewModelFactory;
+
+        public ShowCarsListCommand(ICarsService carsService, INavigator navigator, IViewModelFactory viewModelFactory)
+        {
+            this.carsService = carsService;
+            this.navigator = navigator;
+            this.viewModelFactory = viewModelFactory;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            IEnumerable<Car> cars = await carsService.GetCars();
+            CarsListViewModel carsListViewModel = (CarsListViewModel)viewModelFactory.CreateViewModel(ViewType.CarsList);
+            carsListViewModel.Cars = cars;
+            navigator.CurrentViewModel = carsListViewModel;
+        }
     }
 }
