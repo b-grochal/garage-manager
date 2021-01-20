008bcac3b5c5741775bf24762bbfb523d7e51cbe
Bartosz <radu480@o2.pl>
2021-01-20 23:37:20 +0100

    Implemented command for creating service.

--- GarageManager.UI/Commands/Services/CreateServiceCommand.cs
+++ GarageManager.UI/Commands/Services/CreateServiceCommand.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.Infrastructure;
+using GarageManager.UI.State.Navigator;
+using GarageManager.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.Commands.Services
 {
-    class CreateServiceCommand
+    public class CreateServiceCommand : BaseAsyncCommand
     {
+        private readonly CreateServiceViewModel createServiceViewModel;
+        private readonly IServicesService servicesService;
+        private readonly INavigator navigator;
+        private readonly IViewModelFactory viewModelFactory;
+
+        public CreateServiceCommand(CreateServiceViewModel createServiceViewModel, IServicesService servicesService, INavigator navigator, IViewModelFactory viewModelFactory)
+        {
+            this.createServiceViewModel = createServiceViewModel;
+            this.servicesService = servicesService;
+            this.navigator = navigator;
+            this.viewModelFactory = viewModelFactory;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            await servicesService.CreateService(createServiceViewModel.Service);
+            IEnumerable<Service> services = await servicesService.GetServices();
+            ServicesListViewModel servicesListViewModel = (ServicesListViewModel)viewModelFactory.CreateViewModel(ViewType.ServicesList);
+            servicesListViewModel.Services = services;
+            navigator.CurrentViewModel = servicesListViewModel;
+        }
     }
 }
