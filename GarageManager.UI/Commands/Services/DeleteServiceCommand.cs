9a95e06fbb659c25ca06f7c5d1fef1395070f7b6
Bartosz <radu480@o2.pl>
2021-01-20 23:40:17 +0100

    Implemented command for deleting service.

--- GarageManager.UI/Commands/Services/DeleteServiceCommand.cs
+++ GarageManager.UI/Commands/Services/DeleteServiceCommand.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.Commands.Services
 {
-    class DeleteServiceCommand
+    public class DeleteServiceCommand : BaseAsyncCommand
     {
+        private readonly ServicesListViewModel servicesListViewModel;
+        private readonly IServicesService servicesService;
+
+        public DeleteServiceCommand(ServicesListViewModel servicesListViewModel, IServicesService servicesService)
+        {
+            this.servicesListViewModel = servicesListViewModel;
+            this.servicesService = servicesService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            await servicesService.DeleteService(servicesListViewModel.SelectedService.ServiceId);
+            IEnumerable<Service> services = await servicesService.GetServices();
+            servicesListViewModel.Services = services;
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return !IsExecuting && this.servicesListViewModel.SelectedService != null;
+        }
     }
 }
