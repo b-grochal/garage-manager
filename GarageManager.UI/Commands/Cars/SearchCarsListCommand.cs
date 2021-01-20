fadde18390b8e423dc461fce014fbdbc86a7eb16
Bartosz <radu480@o2.pl>
2021-01-20 11:58:28 +0100

    Implemented command for filtering cars list.

--- GarageManager.UI/Commands/Cars/SearchCarsListCommand.cs
+++ GarageManager.UI/Commands/Cars/SearchCarsListCommand.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GarageManager.UI.Commands.Cars
 {
-    class SearchCarsListCommand
+    public class SearchCarsListCommand : BaseAsyncCommand
     {
+        private readonly CarsListViewModel carsListViewModel;
+        private readonly ICarsService carsService;
+
+        public SearchCarsListCommand(CarsListViewModel carsListViewModel, ICarsService carsService)
+        {
+            this.carsListViewModel = carsListViewModel;
+            this.carsService = carsService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            IEnumerable<Car> filteredCars = await carsService.GetCars(carsListViewModel.CarsListSearchCriteria);
+            carsListViewModel.Cars = filteredCars;
+        }
     }
 }
