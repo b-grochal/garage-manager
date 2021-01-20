430a734929d42a920ebc8493a61b9a3399666fab
Bartosz <radu480@o2.pl>
2021-01-20 22:25:36 +0100

    Prepared details viewmodel for service.

--- GarageManager.UI/ViewModels/Services/ServiceDetailsViewModel.cs
+++ GarageManager.UI/ViewModels/Services/ServiceDetailsViewModel.cs
@@ -1,10 +1,73 @@
-﻿using System;
+﻿using GarageManager.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace GarageManager.UI.ViewModels.Services
 {
-    class ServiceDetailsViewModel
+    public class ServiceDetailsViewModel : BaseViewModel
     {
+        #region Fields
+
+        private Service service;
+
+        #endregion Fields
+
+        #region Properties
+
+        public Service Service
+        {
+            get
+            {
+                return this.service;
+            }
+            set
+            {
+                this.service = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.service.Name;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.service.Description;
+            }
+        }
+
+        public decimal Cost
+        {
+            get
+            {
+                return this.service.Cosr;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return this.service.DateOfService;
+            }
+        }
+
+        #endregion Properties
+
+        #region Constructors
+
+        public ServiceDetailsViewModel()
+        {
+
+        }
+
+        #endregion Constructors
     }
 }
