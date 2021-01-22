91940391720c88e693838b309dcc82c2822683bc
Bartosz <radu480@o2.pl>
2021-01-22 15:08:25 +0100

    Prepared exceptions for auth service.

---
+++ GarageManager.Services/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GarageManager.Services.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public string UserName { get; set; }
+
+        public UserNotFoundException(string username)
+        {
+            UserName = username;
+        }
+
+        public UserNotFoundException(string message, string username) : base(message)
+        {
+            UserName = username;
+        }
+
+        public UserNotFoundException(string message, Exception innerException, string username) : base(message, innerException)
+        {
+            UserName = username;
+        }
+    }
+}
--- GarageManager.Services/Implementation/AuthService.cs
+++ GarageManager.Services/Implementation/AuthService.cs
@@ -1,5 +1,6 @@
 ﻿using GarageManager.Data.Context;
 using GarageManager.Data.Entities;
+using GarageManager.Services.Exceptions;
 using GarageManager.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -13,34 +14,31 @@ namespace GarageManager.Services.Implementation
 {
     public class AuthService : IAuthService
     {
-        private readonly GarageManagerDbContext _garageManagerDbContext;
-        private readonly IPasswordHasher<User> _passwordHasher;
+        private readonly GarageManagerDbContext context;
+        private readonly IPasswordHasher<User> passwordHasher;
 
-        public AuthService(GarageManagerDbContext garageManagerDbContext, IPasswordHasher<User> passwordHasher)
+        public AuthService(GarageManagerDbContext context, IPasswordHasher<User> passwordHasher)
         {
-            this._garageManagerDbContext = garageManagerDbContext;
-            this._passwordHasher = passwordHasher;
+            this.context = context;
+            this.passwordHasher = passwordHasher;
         }
 
         public async Task<User> Login(string userName, string password)
         {
-            User user = await _garageManagerDbContext.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+            User user = await context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
 
             if (user == null)
             {
-                //throw new UserNotFoundException(username);
-                throw new Exception();
+                throw new UserNotFoundException(userName);
             }
 
-            PasswordVerificationResult passwordResult = _passwordHasher.VerifyHashedPassword(user, user.PasswordHash, password);
+            PasswordVerificationResult passwordResult = passwordHasher.VerifyHashedPassword(user, user.PasswordHash, password);
 
             if (passwordResult != PasswordVerificationResult.Success)
             {
-                //throw new InvalidPasswordException(username, password);
-                throw new Exception();
+                throw new InvalidPasswordException(userName);
             }
 
-            //return storedAccount;
             return user;
         }
     }
