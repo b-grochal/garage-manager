13566f9cde05a71cfb6c2638ecd47132759da557
Bartosz <radu480@o2.pl>
2021-01-25 18:46:00 +0100

    Prepared command for resetting search criteria of customers.

---
+++ GarageManager.UI/Commands/Customers/ResetCustomersListSearchCriteriaCommand.cs
@@ -0,0 +1,30 @@
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageManager.UI.Commands.Customers
+{
+    public class ResetCustomersListSearchCriteriaCommand : BaseAsyncCommand
+    {
+        private readonly CustomersListViewModel customersListViewModel;
+        private readonly ICustomersService customersService;
+
+        public ResetCustomersListSearchCriteriaCommand(CustomersListViewModel customersListViewModel, ICustomersService customersService)
+        {
+            this.customersListViewModel = customersListViewModel;
+            this.customersService = customersService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            customersListViewModel.FirstNameSearchCirteria = null;
+            customersListViewModel.LastNameSearchCirteria = null;
+            IEnumerable<Customer> customers = await customersService.GetCustomers();
+            customersListViewModel.Customers = customers;
+        }
+    }
+}
