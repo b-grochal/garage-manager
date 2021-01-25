a9d19dca0b148a21abb96aa6be576f9bf6f37517
Bartosz <radu480@o2.pl>
2021-01-25 18:51:13 +0100

    Prepared command for resetting search criteria of services.

---
+++ GarageManager.UI/Commands/Services/ResetServicesListSearchCriteriaCommand.cs
@@ -0,0 +1,29 @@
+﻿using GarageManager.Data.Entities;
+using GarageManager.Services.Interfaces;
+using GarageManager.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageManager.UI.Commands.Services
+{
+    public class ResetServicesListSearchCriteriaCommand : BaseAsyncCommand
+    {
+        private readonly ServicesListViewModel servicesListsViewModel;
+        private readonly IServicesService servicesService;
+
+        public ResetServicesListSearchCriteriaCommand(ServicesListViewModel servicesListsViewModel, IServicesService servicesService)
+        {
+            this.servicesListsViewModel = servicesListsViewModel;
+            this.servicesService = servicesService;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            servicesListsViewModel.VinSearchCriteria = null;
+            IEnumerable<Service> services = await servicesService.GetServices();
+            servicesListsViewModel.Services = services;
+        }
+    }
+}
