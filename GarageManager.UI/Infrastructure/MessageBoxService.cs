cf3dd7902e7c419770e48487eb1c345417c517ab
Bartosz <radu480@o2.pl>
2021-01-25 19:45:29 +0100

    Prepared service for message boxes.

---
+++ GarageManager.UI/Infrastructure/MessageBoxService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace GarageManager.UI.Infrastructure
+{
+    public class MessageBoxService : IMessageBoxService
+    {
+        public bool ShowConfirmationMessageBox(string title, string content)
+        {
+            MessageBoxResult result = MessageBox.Show(content, title, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            return result == MessageBoxResult.OK ? true : false;
+        }
+
+        public void ShowErrorMessageBox(string title, string content)
+        {
+            MessageBox.Show(content, title, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public void ShowInformationMessageBox(string title, string content)
+        {
+            MessageBox.Show(content, title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
