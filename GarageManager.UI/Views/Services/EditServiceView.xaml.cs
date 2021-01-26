cf4ce4b50436702fb4acf7833aa7b7bb4366eae3
Bartosz <radu480@o2.pl>
2021-01-26 16:19:27 +0100

    Implemented checking characters for input of cost

--- GarageManager.UI/Views/Services/EditServiceView.xaml.cs
+++ GarageManager.UI/Views/Services/EditServiceView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,6 +22,11 @@ namespace GarageManager.UI.Views
         public EditServiceView()
         {
             InitializeComponent();
+        }
+
+        private void CostPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, "^[1-9.,]+$");
         }
     }
 }
