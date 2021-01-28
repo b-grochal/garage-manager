a630528e0f8aef3715c1adc3f27a7e84c4f9ede3
Bartosz <radu480@o2.pl>
2021-01-28 11:22:01 +0100

    Modified starting process.

--- GarageManager.UI/App.xaml.cs
+++ GarageManager.UI/App.xaml.cs
@@ -18,11 +18,11 @@ namespace GarageManager.UI
     /// </summary>
     public partial class App : Application
     {
-        private readonly IHost _host;
+        private readonly IHost host;
 
         public App()
         {
-            _host = CreateHostBuilder().Build();
+            host = CreateHostBuilder().Build();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args = null)
@@ -38,13 +38,17 @@ namespace GarageManager.UI
 
         protected override void OnStartup(StartupEventArgs e)
         {
-            _host.Start();
+            host.Start();
 
             //SimpleTraderDbContextFactory contextFactory = _host.Services.GetRequiredService<SimpleTraderDbContextFactory>();
-            GarageManagerDbContext context = _host.Services.GetRequiredService<GarageManagerDbContext>();
-            context.Database.EnsureCreated();
+            GarageManagerDbContextFactory contextFactory = host.Services.GetRequiredService<GarageManagerDbContextFactory>();
 
-            Window window = _host.Services.GetRequiredService<MainWindow>();
+            using(GarageManagerDbContext context = contextFactory.CreateDbContext())
+            {
+                context.Database.Migrate();
+            }
+            
+            Window window = host.Services.GetRequiredService<MainWindow>();
             window.Show();
 
             base.OnStartup(e);
@@ -52,8 +56,8 @@ namespace GarageManager.UI
 
         protected override async void OnExit(ExitEventArgs e)
         {
-            await _host.StopAsync();
-            _host.Dispose();
+            await host.StopAsync();
+            host.Dispose();
 
             base.OnExit(e);
         }
